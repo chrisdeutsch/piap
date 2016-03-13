a8863b2c466323082d915de3c279bead801c8eb0
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-03-13 17:51:18 +0100

    Uniform proposal

--- code/src/Common.h
+++ code/src/Common.h
@@ -11,6 +11,12 @@ std::function<Particle2D(const Particle2D &, std::mt19937 &)>
 make_proposal_function(double stddev, double box_length);
 
 /**
+ * Uniform out of box with side length 2*delta
+ */
+std::function<Particle2D(const Particle2D &, std::mt19937 &)>
+unif_proposal_function(double delta, double box_length);
+
+/**
  * Creates a state with uniformly distributed particles.
  */
 std::vector<Particle2D> random_state(double box_length, unsigned pair_num);
